25a334ca65e403efe952097e7752bc400f299db2
Kguo-cs <u3006612@connect.hku.hk>
2026-09-07 18:57:42 +0800

    1

--- src/smart/modules/smart_decoder.py
+++ src/smart/modules/smart_decoder.py
@@ -200,7 +200,7 @@ class SMARTDecoder(nn.Module):
 
         self.init_map_encoder = self._make_map_encoder(
             hidden_dim=hidden_dim,
-            pl2pl_radius=pl2pl_radius,
+            pl2pl_radius=10,
             num_freq_bands=num_freq_bands,
             num_layers=1,
             num_heads=num_heads,
