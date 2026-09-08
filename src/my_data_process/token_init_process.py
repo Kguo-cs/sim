ef7d72f698aa94925c171fd40af3402aed1c66ab
Kguo-cs <u3006612@connect.hku.hk>
2026-09-08 15:58:59 +0800

    1

--- src/my_data_process/token_init_process.py
+++ src/my_data_process/token_init_process.py
@@ -34,8 +34,8 @@ token_processor.eval()
 # Set paths
 
 agent_data_directory = "./waymo_data/full/training_map2_03_light"
-map_data_directory  = "./waymo_data/full/training_mapall_init5"
-ouput_data_directory = "./waymo_data/full/training_mapall_init5v"
+map_data_directory  = "./waymo_data/full/training_map2_init5"
+ouput_data_directory = "./waymo_data/full/training_map2_init5v"
 
 
 # agent_data_directory = "/home/ke/code/sim/src/waymo_data/full/validation_map2light"
