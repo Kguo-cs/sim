a28650f8f07406c8358f688402cc9cb07980b002
Kguo-cs <u3006612@connect.hku.hk>
2026-09-09 10:08:03 +0800

    1

--- src/smart/diffusion/scale_flow.py
+++ src/smart/diffusion/scale_flow.py
@@ -815,7 +815,7 @@ class ScaleFlow(nn.Module):
             and branch_mask is not None
             and branch_mask.any()
             and self.token_processor.learn_init
-            and "gt_z_raw" not in tokenized_agent
+            #and "gt_z_raw" not in tokenized_agent
         ):
             noise_level =0.5 #self.get_adaptive_noise_level(time, next_time)
             noise_level = noise_level * branch_mask[:, None].to(latent.dtype)
