d4cc262db915101e6e6880489706f7d69125bd4f
Kguo-cs <u3006612@connect.hku.hk>
2026-09-12 21:52:41 +0800

    1

--- src/smart/model/smart_gail.py
+++ src/smart/model/smart_gail.py
@@ -919,7 +919,7 @@ class SMART_GAIL(SMART):
 
             # Don't let exploration std explode.
             std_loss = (
-                    log_std - math.log(0.1)
+                    log_std - math.log(0.03)
             ).square().mean()
 
             rl_loss = (
